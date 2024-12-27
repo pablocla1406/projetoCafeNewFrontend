f9c351d6f098a0332acd5a65bf2ad604e4eeaf5f
Pablo Oliveira De Arruda <pablocla1406@gmail.com>
2024-12-27 13:40:30 -0300

    13402712

--- frontcafe/src/service/api.ts
+++ frontcafe/src/service/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:3000/',
+    baseURL: 'http://35.198.52.200:3000', // Use o IP do servidor backend com a porta
     timeout: 10000,
     headers: {
-        'Context-type' : 'application/json'
-    }
-})
+        'Content-Type': 'application/json',
+    },
+});
 
-export default api
+export default api;
